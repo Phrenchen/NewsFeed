ea17e887ef4375c5a3c89c817acbfa726c88f80f
Kevin Stähler <phrenchen@gmail.com>
2018-10-05 22:22:18 +0200

    deleted unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NewsFeedComponent } from './news-feed/news-feed.component';
 import { FeedBubbleComponent } from './news-feed/feed-bubble/feed-bubble.component';
 import { TickerComponent } from './news-feed/ticker/ticker.component';
 import { FavouriteBubbleComponent } from './news-feed/favourite-bubble/favourite-bubble.component';
-import { NewsListComponent } from './news-feed/news-list/news-list.component';
 import { DetailsShortComponent } from './news-feed/detail-level/details-short/details-short.component';
 import { DetailsTeaserComponent } from './news-feed/detail-level/details-teaser/details-teaser.component';
 import { DetailsLongComponent } from './news-feed/detail-level/details-long/details-long.component';
@@ -18,7 +17,6 @@ import { DetailsLongComponent } from './news-feed/detail-level/details-long/deta
     FeedBubbleComponent,
     TickerComponent,
     FavouriteBubbleComponent,
-    NewsListComponent,
     DetailsShortComponent,
     DetailsTeaserComponent,
     DetailsLongComponent
