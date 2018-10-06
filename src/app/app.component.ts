9bb2e4197c29634456aefb482951042e793f20ea
Kevin Stähler <phrenchen@gmail.com>
2018-10-06 10:58:20 +0200

    edited build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, enableProdMode } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'news-feed';
 
-  
+  constructor() {
+    // enableProdMode();
+  }
 }
