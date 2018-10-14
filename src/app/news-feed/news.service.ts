f702eaaf679a164981f08591ffa50fa254ac8882
Kevin Stähler <phrenchen@gmail.com>
2018-10-14 21:01:30 +0200

    extracting helper method for base url

--- src/app/news-feed/news.service.ts
+++ src/app/news-feed/news.service.ts
@@ -7,7 +7,16 @@ import { NewsItem } from './model/NewsItem';
 })
 export class NewsService {
 
+  public static NEWS = 'news';
+
   @Output() newsUpdate = new EventEmitter< string >();
+
+  public static getEndPointBase(): string {
+    return (location.hostname === 'localhost' || location.hostname === '127.0.0.1')
+    ? 'http://localhost:8080/api/'
+    : 'api/';
+  }
+
 
   constructor() { }
 
@@ -15,12 +24,11 @@ export class NewsService {
    * returns NewsItem[]
    */
   public requestNews(): Promise<any> {
-    const endpoint = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') 
-      ? 'http://localhost:8080/api/news'
-      : 'api/news';
+    const endpoint = NewsService.getEndPointBase() + NewsService.NEWS;
+
     console.log('requesting news: ' + endpoint);
+
     return axios.get(endpoint)
-    // return axios.get('/api/news')
     .then((response: AxiosResponse<any>) => {
         console.log('received news');
         return response.data;
