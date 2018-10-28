0a7f5db5b45ed16815688499830b9579cbc262d6
Kevin Stähler <phrenchen@gmail.com>
2018-10-28 14:18:21 +0100

    adding optional auth data

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 const path = require('path');
-var app = express();
-var pgp = require('pg-promise')(/* options */);
-// var db = pgp('postgres://username:password@host:port/database');
-var db = pgp("postgres://defaultuser:1u2MtKAZBHouW5H2FWg1@localhost:5432/NewsFeed");
 const uuid = require('uuid');
+var pgp = require('pg-promise')(/* options */);
+
+var app = express();
+// var db = pgp('postgres://username:password@host:port/database');
+// var dbUsername = process.env.DB_USER_NAME;  // defaultuser
+// var dbPassword = process.env.DB_PASSWORD;   // 1u2MtKAZBHouW5H2FWg1
+// var db = pgp("postgres://defaultuser:1u2MtKAZBHouW5H2FWg1@localhost:5432/NewsFeed");
+
+
+var dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://defaultuser:1u2MtKAZBHouW5H2FWg1@localhost:5432/NewsFeed";
+var db = pgp(dbUrl);
 app.use(express.static(__dirname + "/dist/news-feed"));
 
 
@@ -45,8 +52,9 @@ async function getNews() {
 async function addNews(title, shortDescription, longDescription, thumbnail, images) {
     const id = uuid();
     
-    return db.none('INSERT INTO news(id, title, shortDescription, longDescription, thumbnail, images) VALUES($1, $2, $3, $4, $5, $6)', [id, title, shortDescription, longDescription, thumbnail, images])
-        .then(() => {
+    return db.none('INSERT INTO news(id, title, shortDescription, longDescription, thumbnail, images) VALUES($1, $2, $3, $4, $5, $6)', 
+        [id, title, shortDescription, longDescription, thumbnail, images]
+        ).then(() => {
             // success;
             console.log("success adding newsitem!");
             return true;
@@ -79,7 +87,6 @@ app.get('/api/news', (req, res) =>{
             console.log("received result: " + result);
             console.log(result.length);
             res.setHeader('Access-Control-Allow-Origin', '*');
-            // res.send(responseData);
             res.send(result);
         });
 });
@@ -87,74 +94,23 @@ app.get('/api/news', (req, res) =>{
 
 
 async function initializeNews() {
-    let newNews;
-
     await deleteOldNews();
-    newNews = await getNews();
-    console.log("total news after deleting: " + newNews.length);
-    await addNews("news numma 1!", "aaaaaldaaaaa short description adla!", "long description", "thumbnail", []);
-    console.log("1 complete");
-    await addNews("news numma 2!", "aaaaaldaaaaa short description adla!", "long description", "thumbnail", []);
-    console.log("2 complete");
+    await addNews(
+        "cat content", 
+        "interested in cat content?", 
+        '<h2>Cats!</h2> <img draggable="false" onload="console.log(`script executed! alert!`)" src="assets/images/happy_cat.jpg" alt="cat!" style="width:100%;">', 
+        "/assets/images/happy_cat.jpg", 
+        ["explosionkitty.jpeg", "lazycat.png", "mr_mustache.jpg", "ohnoes.jpg", "surprisedcat3.jpg", "tshirt.jpeg"]
+        );
+    await addNews(
+        "AI plays Snake", 
+        "youtube", 
+        '<div><a href="https://www.youtube.com/watch?v=3bhP7zulFfY" target="_blank">AI plays Snake</a> </div>', 
+        "/assets/images/ai_snake_game.png", 
+        []
+        );
     await addNews("news numma 3!", "aaaaaldaaaaa short description adla!", "long description", "thumbnail", []);
-    console.log("3 complete");
-    newNews = await getNews();
-    console.log("total news: " + newNews.length);
-    
 }
 
+// run on start
 initializeNews();
-console.log("setting it up");
-
-// .then(()=>{
-//     getNews();
-// })
-// .then(news =>{
-//     console.log("got new news: " + news); 
-
-// });
-
-// addNews("news numma 2!", "und noch eine wie geiiiil");
-
-// ------------------------------------------------------------------------
-// static news feed 
-const responseData = [{
-    id: '1',
-    isRead: false,
-    isFavourite: true,
-
-    // content meta
-    dateCreated: new Date(),
-    dateUpdated: new Date(),
-    dateRead: new Date(),
-    seoDescritpion: 'seo tags',
-    sortOrder: 1,
-
-    // content
-    title: 'cat content',                      // HTML
-    shortDescription: 'interested in cat content?',    // HTML
-    longDescription: '<h2>Cats!</h2> <img draggable="false" onload="console.log(`script executed! alert!`)" src="assets/images/happy_cat.jpg" alt="cat!" style="width:100%;">',
-    thumbnail: "/assets/images/happy_cat.jpg",
-    images: ["explosionkitty.jpeg", "lazycat.png", "mr_mustache.jpg", "ohnoes.jpg", "surprisedcat3.jpg", "tshirt.jpeg" ]
-  },
-  {
-    id: '2',
-    isRead: false,
-    isFavourite: false,
-
-    // content meta
-    dateCreated: new Date(),
-    dateUpdated: new Date(),
-    dateRead: new Date(),
-    seoDescritpion: 'seo tags',
-    sortOrder: 2,
-
-    // content
-    title: 'AI plays Snake',                      // HTML
-    shortDescription: 'video links',    // HTML
-    longDescription: '<div><a href="https://www.youtube.com/watch?v=3bhP7zulFfY" target="_blank">AI plays Snake</a> </div>',       // HTML
-    thumbnail: "assets/images/ai_snake_game.png",
-    images: []
-  }
-];
-
