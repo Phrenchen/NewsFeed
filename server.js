1ef31f80bff6154de774e0d19bb28c21575a5496
Kevin Stähler <phrenchen@gmail.com>
2018-11-04 22:46:06 +0100

    code clean up

--- server.js
+++ server.js
@@ -3,28 +3,17 @@ const path = require('path');
 const uuid = require('uuid');
 const { Pool } = require('pg');
 var pgp = require('pg-promise')(/* options */);
-
-
 var app = express();
-// var db = pgp('postgres://username:password@host:port/database');
-// var dbUsername = process.env.DB_USER_NAME;  // defaultuser
-// var dbPassword = process.env.DB_PASSWORD;   // 1u2MtKAZBHouW5
-// var db = pgp("postgres://defaultuser:1u2MtKAZBHouW5@localhost:5432/NewsFeed");
 
 
 app.use(express.static(__dirname + "/dist/news-feed"));
 
-// postgres://yfsawoxbctupqs:1a30c5b81059ff3833c58c18b998a6e2535c0d20063e5b9e7b395529ab616a6a@ec2-79-125-8-105.eu-west-1.compute.amazonaws.com:5432/d1l94i9ms624ph
 var dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://defaultuser:1u2MtKAZBHouW5@localhost:5432/NewsFeed";
-console.log("db url:  " + dbUrl);
-// var db = pgp(dbUrl);
 
 const pool = new Pool({
     connectionString: dbUrl,
     ssl: false
 });
-
-
 
 
 // listen
@@ -35,7 +24,6 @@ app.listen(process.env.PORT || port, () => {
 });
 
 app.get('/', (req, res) => {
-    // console.log("get root"); 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.sendFile(path.join(__dirname, '/dist/news-feed/index.html'));
 });
@@ -47,43 +35,22 @@ app.get('/api/news', async (req, res) =>{
     const result = await client.query('SELECT * FROM news');
     client.release();
     const newsResults = result.rows;
-    console.log(newsResults);
+    // console.log(newsResults);
     res.send(newsResults);
     console.log("sent and released. done!");
 });
-    // getNews()
-    //     .then(result =>{
-    //         console.log("received result: " + result);
-    //         console.log(result.length);
-    //         res.setHeader('Access-Control-Allow-Origin', '*');
-    //         res.send(result);
-    //     });
-    // });
-    
-
 
 
 
 // helper
 // POSTGRESQL
 async function initDB() {
-    // return await db.any(`DROP TABLE news CREATE TABLE news`)
     try{
-
-        console.log("initializing db");
+        // console.log("initializing db");
         const client = await pool.connect();
-        console.log("connected");
+        // console.log("connected");
         await client.query(`DROP TABLE IF EXISTS news;`);
-        console.log("dropped table news");
-        // try{
-        //     await client.query(`CREATE USER defaultuser WITH CREATEDB`);
-        // }
-        // catch(e){
-        //     console.log("error creating user.");
-        //     console.log(e);
-        // }
-        // console.log("created user");
-
+        // console.log("dropped table news");
         await client.query(`CREATE TABLE news
         (
             id uuid NOT NULL,
@@ -95,10 +62,10 @@ async function initDB() {
             CONSTRAINT news_id PRIMARY KEY (id)
             );
             `);
-        console.log("created table news");
+        // console.log("created table news");
         
         await client.release();
-        console.log("released client");
+        // console.log("released client");
     }
     catch(e){
         console.log("error initializing db.");
@@ -107,72 +74,26 @@ async function initDB() {
 
 }
 
-async function getNews() {
-    try{
-        const client = await pool.connect();
-        const result = await client.query(`SELECT * FROM news`)
-        // console.log(result);
-        // console.log(result.fields);
-        console.log(result.rows.length);
-        // return await db.any('SELECT * FROM news', [true])
-            // .then(data => {
-            //     // success
-            //     console.log("got news from postgres!");
-            //     console.log(data.length);
-            //     return data;
-            // }).catch(e => {
-            //     console.log("error catch ");
-            // });
-    }
-    catch(e){
-        console.log("error calling postgresq");
-        console.log(e);
-        return [];
-    }
-    
-}
-// addNews("news numma 1!", "aaaaaldaaaaa short description adla!", "long description", "thumbnail", []);
 async function addNews(title, shortDescription, longDescription, thumbnail, images) {
     const id = uuid();
     const client = await pool.connect();
-    // console.log(images);
-    const result = await client.query("INSERT INTO news(id, title, shortDescription, longDescription, thumbnail, images) VALUES('" + id + "','" + title + "','" + shortDescription + "','" + longDescription + "','" + thumbnail + "','{" + images + "}')")
+    await client.query("INSERT INTO news(id, title, shortDescription, longDescription, thumbnail, images) VALUES('" + id + "','" + title + "','" + shortDescription + "','" + longDescription + "','" + thumbnail + "','{" + images + "}')")
     .then(() => {
-        // success;
-        console.log("success adding newsitem!");
         client.release();
         return true;
     })
     .catch(error => {
-        // error;
         console.log("error adding newsitem!");
         console.log(error);
         client.release();
         return false;
     });
-    /*
-    return db.none('INSERT INTO news(id, title, shortDescription, longDescription, thumbnail, images) VALUES($1, $2, $3, $4, $5, $6)', 
-        [id, title, shortDescription, longDescription, thumbnail, images]
-        ).then(() => {
-            // success;
-            console.log("success adding newsitem!");
-            return true;
-        })
-        .catch(error => {
-            // error;
-            console.log("error adding newsitem!");
-            console.log(error);
-            return false;
-        });
-    */
 }
 // POSTGRESQL end
 
 
 async function initializeNews() {
     await initDB();
-    console.log("deleted old news");
-    await getNews();
     await addNews(
         "cat content", 
         "interested in cat content?", 
@@ -189,7 +110,6 @@ async function initializeNews() {
             
         );
     await addNews("news numma 3!", "aaaaaldaaaaa short description adla!", "long description", "thumbnail", []);
-    await getNews();
 }
 
 // run on start
